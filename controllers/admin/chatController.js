d130b8d800b9e938a515fbb797c240ba7b79f1d7
sagar03d <sagar03d@gmail.com>
2021-06-22 22:52:41 +0530

    chat improvement

--- controllers/admin/chatController.js
+++ controllers/admin/chatController.js
@@ -1,11 +1,58 @@
 const catchAsync = require('../catchAsync');
 const User = require('../../models/User');
+const { WebClient, LogLevel } = require("@slack/web-api");
+const slackToken = process.env.slackToken;
+const channelId = "C0261NR59ED";
 
-const index = (async (req, res) => {
-  res.render('admin/chat');
+const index = catchAsync(async (req, res) => {
+
+    const client = new WebClient(slackToken, {
+      // LogLevel can be imported and used to make debugging simpler
+      logLevel: LogLevel.DEBUG
+    });
+
+    let conversationHistory;
+
+    try {
+      // Call the conversations.history method using WebClient
+      const result = await client.conversations.history({
+        channel: channelId
+      });
+
+      conversationHistory = result.messages;
+
+      // Print results
+      res.render('admin/chat',{chats:conversationHistory,chatlength:conversationHistory.length});
+      // res.send(conversationHistory.length + " messages found in " + channelId);
+    }
+    catch (error) {
+      console.error(error);
+    }
+
+  
 });
 
+const sendMessage = catchAsync(async (req, res) => {
+    let msg = req.body.text;
+
+    const client = new WebClient(slackToken, {
+      logLevel: LogLevel.DEBUG
+    });
+
+    try {
+      // Call the chat.postMessage method using the WebClient
+      const result = await client.chat.postMessage({
+        channel: channelId,
+        text: msg
+      });
+      res.json({success:true,data:result})
+    }
+    catch (error) {
+      console.error(error);
+    }
+});
 
 module.exports = {
-  index
+  index,
+  sendMessage
 }
--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 const loginController = require('../controllers/admin/loginController');
 const homeController = require('../controllers/admin/homeController');
+const chatController = require('../controllers/admin/chatController');
 var auth = require('../middleware/auth');
 
 router.get('/', loginController.index);
 router.post('/login', loginController.login);
 router.get('/home', auth.isAuthorized, homeController.index);
+router.get('/chat', auth.isAuthorized, chatController.index);
+router.post('/sendMessage', auth.isAuthorized, chatController.sendMessage);
 
 module.exports = router;
